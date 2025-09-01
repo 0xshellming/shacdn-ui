1c50c402ecfafe655a1c5122b94d5142c1ade464
shellming <xm726@qq.com>
2025-09-01 16:17:50 +0800

    feat: add TypeScript definitions for AMP auto ads component

---
+++ apps/www/types/amp.d.ts
@@ -0,0 +1,10 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'amp-auto-ads': {
+      type?: string
+      'data-ad-client'?: string
+      children?: React.ReactNode
+    }
+  }
+}
+
